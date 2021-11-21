2c2447e296ac9a64211a91187fbde8a22559c11c
Privanom <70576558+Privanom@users.noreply.github.com>
2021-11-21 00:24:38 -0800

    Made additional if statements into else-if statements

--- bubblesort.c
+++ bubblesort.c
@@ -16,56 +16,56 @@ int main()
     };
    
     //Swaps yarr[0] and yarr[1] 
-    if(yarr[0] < yarr[1])
+    else if(yarr[0] < yarr[1])
     {
         const int yarr_0 = yarr[0];
         yarr[0] = yarr[1];
         yarr[1] = yarr_0;
     }
     
-    if(yarr[2] < yarr[3])
+    else if(yarr[2] < yarr[3])
     {
         const int yarr_2 = yarr[2];
         yarr[2] = yarr[3];
         yarr[3] = yarr_2;
     }
     
-    if(yarr[3] < yarr[4])
+    else if(yarr[3] < yarr[4])
     {
         const int yarr_3 = yarr[3];
         yarr[3] = yarr[4];
         yarr[4] = yarr_3;
     }
     
-    if(yarr[4] < yarr[5])
+    else if(yarr[4] < yarr[5])
     {
         const int yarr_4 = yarr[4];
         yarr[4] = yarr[5];
         yarr[5] = yarr_4;
     }
     
-    if(yarr[5] < yarr[6])
+    else if(yarr[5] < yarr[6])
     {
         const int yarr_5 = yarr[5];
         yarr[5] = yarr[6];
         yarr[6] = yarr_5;
     }
     
-    if(yarr[6] < yarr[7])
+    else if(yarr[6] < yarr[7])
     {
         const int yarr_6 = yarr[6];
         yarr[6] = yarr[7];
         yarr[7] = yarr_6;
     }
     
-    if(yarr[7] < yarr[8])
+    else if(yarr[7] < yarr[8])
     {
         const int yarr_7 = yarr[7];
         yarr[7] = yarr[8];
         yarr[8] = yarr_7;
     }
     
-    if(yarr[8] < yarr[9])
+    else if(yarr[8] < yarr[9])
     {
         const int yarr_8 = yarr[8];
         yarr[8] = yarr[9];

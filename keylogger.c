5abc167b53706ab6e4bc182e4d17d2e0e61636bd
Privanom <70576558+Privanom@users.noreply.github.com>
2021-11-21 18:15:27 -0800

    Create keylogger.c
    This is a basic algorithm that writes all characters pressed into keylogger.txt (which already has to be a file).

---
+++ keylogger.c
@@ -0,0 +1,29 @@
+//I am not responsible for the use of this algorithm for illegal purposes, as it is intended for education :).
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE * fPtr;
+    fPtr = fopen("data/file1.txt", "w");
+    
+    if(fPtr == NULL)
+    {
+        exit(1);
+    }
+    
+    fopen("keylogger.txt", "w");
+    int a = getchar();
+    getchar();
+    
+    for(int i = 1; i > 0; i++)
+    {
+        fprintf(a, fPtr);
+    }
+    
+    if(i == 0)
+    {
+        fclose("kelogger.txt");
+        exit(1);
+    }
+}

b0f138ba3b678b646260986b31ea78efc2de327b
zag8 <git.dp@inbox.lv>
2021-11-24 09:50:38 +0100

    convert indentation to spaces

--- bubblesort.c
+++ bubblesort.c
@@ -3,25 +3,25 @@
 void bubble_sort(int *array, int len) 
 {
     for (int i = 0; i < len; i++) {
-		for (int j = 0; j < len - i - 1; j++) {
-			if (array[j] > array[j + 1]) {
-				int tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-			}
-		}
-	}
+        for (int j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
 }
 
 int main(void) 
 {
-	int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
     
-	bubble_sort(yarr, 10);
+    bubble_sort(yarr, 10);
 
-	printf("[");
-	for (int i = 0; i < 10; i++)
-		printf("%d, ", yarr[i]);	
-	printf("\b\b]\n");
-	return 0;
+    printf("[");
+    for (int i = 0; i < 10; i++)
+        printf("%d, ", yarr[i]);	
+    printf("\b\b]\n");
+    return 0;
 }

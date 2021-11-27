55b704ce1d72bdca5e6fed1e5b2489fa31ca009b
Privanom <70576558+Privanom@users.noreply.github.com>
2021-11-27 00:42:25 -0800

    Create linearsearch.c
    A complete linear search in 24 lines! Ignore the warnings.

---
+++ linearsearch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int linsearch(int *yarr, int val)
+{
+    for(int i = 0; i < sizeof(yarr); i++)
+    {
+        if(yarr[i] == val)
+        {
+            return 0;
+        }    
+        else
+        {
+          return 1;
+        }
+    }
+}
+
+int main()
+{
+    int *yarr = {"Shluck much", "much Shluck"};
+    int val = "Shluck Much";
+    linsearch(yarr, val);
+}

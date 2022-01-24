cd1611a4693fa2122266bc7b63fa6f2daa6fb56d
Privanom <70576558+Privanom@users.noreply.github.com>
2022-01-23 18:50:49 -0800

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,24 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
-int mergesort(int size_, int arg_[])
+// function to merge the sub-arrays
+void merge(int a[],int low,int mid ,int high)
 {
-   for(int i = 0; i < size_; i++)
-    {
-        // A is just used for the comparison, the for loop is complicated for debugging purposes
-        int a;
-        for(a = 1; a < size_; a--)
-            a += 4;
-             
-        if(arg_[i] > arg_[a])
-            arg_[i] = arg_[a];
-    }
-    return 0;
+	int b[20]; //same size of a[]
+	int i, j, k;
+	i = low, j = mid + 1,k = low;
+	while(i <= mid && j <= high)
+	{
+		if(a[i]<=a[j])
+		    b[k++]=a[i++];
+		else
+		   b[k++]=a[j++]; //copying the elements 
+	}
+	while (i<=mid)
+		b[k++]=a[i++];
+	while 
+		(j<=high) b[k++]=a[j++];
+		for (k=low;k<=high;k++)
+	        a[k]=b[k];
 }
 
+// merge sort function
+void mergesort(int a[],int low,int high)
+{
+	int mid;
+	if(low>=high)
+	  return;
+	mid=(low+high)/2;
+	mergesort(a,low,mid);
+	mergesort(a,mid+1,high);
+	merge(a,low,mid,high);
+}
+
+// main fucntion
 int main()
 {
-    int yarr[] = {12, 4, 8, 9, 20};
-    int size_ = sizeof(yarr);
-    mergesort(size_, yarr);
+	int a[7] = {83, 20, 9, 50, 115, 61, 17};
+	int n = 7;
+
+	mergesort(a, 0, n-1);
+	
+	printf("\n Sorted numbers are: ");
+
+	// function to print the sorted array
+	int k;
+	for(k = 0; k < 7; k++)
+	    printf("%d, ",a[k]);
+	return 0;
 }

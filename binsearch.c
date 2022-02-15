7f3daa3ba73c68b19589e62a4e9685e5a6a5c45f
Privanom <70576558+Privanom@users.noreply.github.com>
2022-02-15 02:16:14 -0800

    Change binsearch.c to be used via command line

--- binsearch.c
+++ binsearch.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 
-int main()
+int binsearch(int yarr[10], int element)
 {
-    int yarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    int element;
     int mid = sizeof(yarr) % 2;
     int left = (int)yarr / 2 - mid;
     int right = yarr - left;
     extern int i;
     
-    int searchend()
-    {
-        return i;
-    }
-    
-    printf("Binary search:~$ ");
-    scanf("%d", &element);
-    
     if(element == mid)
     printf("%d", &mid);
     
-    else if(element != yarr[right])
+    if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
             if(element == i)
             {
             //Does this so that it doesn't print multiple times
-            searchend(i);
-            i = element;
+            printf("%d", i);
             }
         }
     }
 };
+
+int main(int argc, int argv[])
+{
+    binsearch(argv[0], argv[1]);
+}

1b945639cc786ec841ba948fc7ee26ccab2eb7fe
Tuhin Dhar <tuhindhar014@gmail.com>
2022-02-19 15:40:56 +0530

    Add quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b);
+int partition(int *a, int l, int h);
+void quick_sort(int *a, int l, int h);
+
+int main()
+{
+    int size;
+    printf("Size: ");
+    scanf("%d", &size);
+
+    int *arr = malloc(size * sizeof(int));
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    quick_sort(arr, 0, size - 1);
+
+    printf("Sorted array:");
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return;
+}
+
+int partition(int a[], int l, int h)
+{
+    int i = l, j = l, p = h;
+
+    while (i < h)
+    {
+        if (a[i] < a[p])
+        {
+            swap(&a[i], &a[j]);
+            j++;
+        }
+        i++;
+    }
+
+    swap(&a[p], &a[j]);
+
+    return j;
+}
+
+void quick_sort(int a[], int l, int h)
+{
+    int p;
+
+    if (l < h)
+    {
+        p = partition(a, l, h);
+        quick_sort(a, l, p - 1);
+        quick_sort(a, p + 1, h);
+    }
+
+    return;
+}

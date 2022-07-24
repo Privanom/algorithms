491cecb1bb8f7b506e7a64d7eeaa730b60a2c64e
Sam <iamthebadwolf80@gmail.com>
2022-07-24 09:58:34 -0400

    Made even_or_odd.c more concise

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+//Check whether 'number' is even or odd and print the result
+void evenOrOdd(long number)
+{
+    number & 1 ? printf("Odd\n") : printf("Even\n");
+}
+
 int main()
 {
+    //Retrieve number from user
     long num;
-    printf("Enter a number\n");
+    printf("Enter a number: ");
     scanf("%ld", &num);
-    int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
-    else
-    printf("Odd");
+    //Call evenOrOdd on 'num'
+    evenOrOdd(num);
+    return 0;
 }
